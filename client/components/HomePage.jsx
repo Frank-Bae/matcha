d07a6fbeab979acd62a273e7009e927b925bb922
Frank <50495924+Bae26@users.noreply.github.com>
2020-07-29 21:25:46 -0700

    figured out way to add image to website

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Carousel = styled.div`
+  img {
+  width: 375px;
+  height: 400px;
+  object-fit: cover;
+}
+`;
 
 export default class HomePage extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>Can</h2>
-      </div>
+      <Carousel>
+        <img src="./images/matcha.png" alt=""/>
+      </Carousel>
     );
   }
 }
